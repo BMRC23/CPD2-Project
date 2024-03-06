860bea686d09c592002d366ae09f9b77b3ade7c5
BMRC23 <127077350+BMRC23@users.noreply.github.com>
2024-03-06 14:06:57 +0800

    Delete EmpFilesRep directory (maling code)

--- EmpFilesRep/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.empfilesrep {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.empfilesrep to javafx.fxml;
-    exports com.example.empfilesrep;
-}
